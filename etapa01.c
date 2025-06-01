9e914d3a01e310bb08bb6dcfc4d3553852803453
Camila Parolin <camilaparolin10@gmail.com>
2025-06-01 16:55:49 -0300

    "Remove unnecessary files"

--- etapa01.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "simulator.h"
-
-// Memory Paging Simulator: Simulates paging in an operating system with virtual memory management.
-int main() {
-    printf("Memory Paging Simulator\n");
-    return 0;
-}
